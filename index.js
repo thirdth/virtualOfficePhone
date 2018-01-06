55ace3bd1a2f6e3466e9cf1e3ba2b707d2418d7a
Zack Glaser <zackglaser@gmail.com>
2018-01-05 19:14:09 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var Promise = require('bluebird');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bandwidth = require("node-bandwidth");
+var xml = bandwidth.xml;
+
+var appName = "Reroute to Zack Cell";
+var client = new bandwidth({
+  userId    : "u-u53rntsssijiu5tp4e453li",  // note, this is not the same as the username you used to login to the portal
+  apiToken  : "t-2yqgf62yrnwqkjio3wc72ba",
+  apiSecret : "k5sih5b3m3evj6k46267cpzk7c3k7pipulimpfi"
+});
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('static'));
+app.post('/incomingCall', function(req, res) {
+		client.Call.answer("callID").then(function () {});
+});
