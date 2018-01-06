d2bd72606f45ecf6c0e08715c713b5f3737bd565
Zack Glaser <zackglaser@gmail.com>
2018-01-05 19:59:54 -0600

    added configure app etc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bandwidth = require("node-bandwidth");
 var xml = bandwidth.xml;
-var tn = "16152084943";
+var tn;
 
 var appName = "Reroute to Zack Cell";
 var client = new bandwidth({
@@ -23,6 +23,69 @@ response.push(speakSentence);
 // Create the xml to send
 var bxml = response.toXml();
 
+//Checks the current Applications to see if we have one.
+var configureApplication = function () {
+	return Application.listAsync(client, {
+		size: 1000
+	})
+	.then(function (applications) {
+		var applicationId = searchForApplication(applications, appName);
+		if(applicationId !== false) {
+			return fetchTNByAppId(applicationId);
+		}
+		else {
+			console.log("no app");
+		}
+	});
+};
+
+// Searches through application names and returns ID if matched
+var searchForApplication = function (applications, name) {
+	for (var i = 0; i < applications.length; i++) {
+			if ( applications[i].name === name) {
+				return applications[i].id;
+			}
+		}
+	return false;
+};
+
+// Gets the first number associated with an application
+var fetchTNByAppId = function (applicationId) {
+	return PhoneNumber.listAsync(client, {
+		applicationId: applicationId
+	})
+	.then(function (numbers) {
+		tn = numbers[0].number;
+	});
+};
+
+// Creates a new application then orders a number and assigns it to application
+var newApplication =function () {
+	var applicationId;
+	return Application.createAsync(client, {
+			name: appName,
+			incomingCallUrl: "https://www.tel.tech4lawyers.com/incomingCall/",
+			callbackHttpMethod: "get",
+			autoAnswer: true
+		})
+		.then(function(application) {
+			//search an available number
+			applicationId = application.id;
+			return AvailableNumber.searchLocalAsync(client, {
+				areaCode: "415",
+				quantity: 1
+			});
+		})
+		.then(function(numbers) {
+			// and reserve it
+			tn = numbers[0].number;
+			return PhoneNumber.createAsync(client, {
+				number: tn,
+				applicationId: applicationId
+			});
+		});
+};
+
 app.use(express.static('static'));
 app.get('/incomingCall', function(req, res) {
 	if(req.query && req.query.eventType && req.query.eventType === 'answer') {
@@ -34,4 +97,15 @@ app.get('/incomingCall', function(req, res) {
 	else {
 		res.send({status: 200});
 	}
+});
+
+configureApplication()
+.then(function () {
+	io.on('connection', function(socket){
+		socket.emit('connected', 'Connected!');
+	});
+
+	http.listen(app.get('port'), function(){
+		console.log('listening on *:' + app.get('port'));
+	});
 });
