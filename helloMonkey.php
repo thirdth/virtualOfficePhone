25e4d8e58232e2997f63c5ef8254b5bc30edeb23
Zack Glaser <zackglaser@gmail.com>
2018-01-11 10:57:54 -0600

    added tranfer to another number

--- helloMonkey.php
+++ helloMonkey.php
@@ -3,6 +3,8 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>Hello Monkey</Say>
-    <Say>Why are you calling me</Say>
+    <Say>You have reached tech for lawyers.</Say>
+    <Gather numDigits="1" action="hello-monkey-transfer.php" metod="POST">
+      <Say>To speak to a representative, please press 1.</Say>
+    </Gather>
 </Response>
