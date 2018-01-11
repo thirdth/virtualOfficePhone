ec8417ded88a8ce390e8a45666e00a33322defe9
Zack Glaser <zackglaser@gmail.com>
2018-01-11 17:06:32 -0600

    debug call recordings

--- messages.php
+++ messages.php
@@ -16,6 +16,7 @@ $client = new Client($TWsid, $TWtoken);
 }*/
 
 foreach ($client->calls->read() as $call) {
+  print_r($client);
   echo "<div>
           <a href='https://api.twilio.com/2010-04-01/Accounts/". $TWsid . "/Recordings/" . $call->sid . "'>" . $call->from . "</a>
         </div>";
