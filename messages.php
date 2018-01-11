3c3ccf7f3dba429f0e89f8437574252dc9e23e8e
Zack Glaser <zackglaser@gmail.com>
2018-01-11 13:54:12 -0600

    remove token

--- messages.php
+++ messages.php
@@ -1,12 +1,10 @@
 <?php
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
+require_once '/../config/config.php'
 require_once '/path/to/vendor/autoload.php'; // Loads the library
 use Twilio\Rest\Client;
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACc08b43ed3bd68082adf012d836f2dc83";
-$token = "d66915e66058d445d81907a34f021e2f";
-$recording_sid = "RE557ce644e5ab84fa21cc21112e22c485";
 $client = new Client($sid, $token);
 
 // Loop over the list of recordings and echo a property for each one
