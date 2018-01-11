06ffffa796c8307e87b1024ff6ed2afdb190cea4
Zack Glaser <zackglaser@gmail.com>
2018-01-11 15:57:25 -0600

    added some div

--- messages.php
+++ messages.php
@@ -9,6 +9,8 @@ $client = new Client($TWsid, $TWtoken);
 
 // Loop over the list of recordings and echo a property for each one
 foreach ($client->recordings->read() as $recording) {
-    echo "<a href='https://api.twilio.com/2010-04-01/Accounts/". $TWsid . "/Recordings/" . $recording->sid . "'>" . $recording->sid . "</a>";
+    echo "<div>
+            <a href='https://api.twilio.com/2010-04-01/Accounts/". $TWsid . "/Recordings/" . $recording->sid . "'>" . $recording->sid . "</a>
+          </div>";
 }
 ?>
