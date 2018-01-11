3b7ace02bffe3f0ab6b9cf28a70451c504ebf7c5
Zack Glaser <zackglaser@gmail.com>
2018-01-11 16:49:02 -0600

    debug recording array

--- messages.php
+++ messages.php
@@ -6,13 +6,12 @@ use Twilio\Rest\Client;
 
 // Your Account Sid and Auth Token from twilio.com/user/account
 $client = new Client($TWsid, $TWtoken);
-print_r($client);
 
 // Loop over the list of recordings and echo a property for each one
-foreach ($client->read() as $message) {
-  //  print_r($message);
-  /*  echo "<div>
+foreach ($client->recordings->read() as $recording) {
+    print_r($recording);
+    echo "<div>
             <a href='https://api.twilio.com/2010-04-01/Accounts/". $TWsid . "/Recordings/" . $recording->sid . "'>" . $recording->sid . "</a>
-          </div>";*/
+          </div>";
 }
 ?>
