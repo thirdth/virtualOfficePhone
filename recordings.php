ee9b2f37ef103397e70da3a31e83b889ec469409
Zack Glaser <zackglaser@gmail.com>
2018-01-11 21:20:42 -0600

    recordings debug

--- recordings.php
+++ recordings.php
@@ -16,7 +16,7 @@ $recordings = $client->recordings->read(
     )
 );
 
-print_r($client);
+print_r($client->recordings);
 // Loop over the list of recordings and echo a property for each one
 foreach ($recordings as $recording) {
     echo $recording;
