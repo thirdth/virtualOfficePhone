425e9d0e177fa7d41a388417ab136a5a447bf4cd
Zack Glaser <zackglaser@gmail.com>
2018-01-11 21:24:50 -0600

    call sid

--- recordings.php
+++ recordings.php
@@ -16,7 +16,7 @@ $recordings = $client->recordings->read(
     )
 );
 
-print_r($client->recordings);
+print_r($client->recordings->callSid);
 // Loop over the list of recordings and echo a property for each one
 foreach ($recordings as $recording) {
     echo $recording;
