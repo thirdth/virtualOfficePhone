5a5894a2d3d107fa6c57ba434a84fca686c95fb6
Zack Glaser <zackglaser@gmail.com>
2018-01-11 22:22:33 -0600

    add read

--- messages.php
+++ messages.php
@@ -20,7 +20,7 @@ foreach ($client->calls->read() as $call) {
   echo "  |  ";
   print_r($call->recordings);
   echo "  |  ";
-  print_r($call->recordings->uri);
+  print_r($call->recordings->read());
   echo "<div>
           <p>" . $call->direction . " | " . $call->startTime->date . "</p>
           <a href='recordings.php?callSid=" . $call->sid . "'>" . $call->sid . "</a>
