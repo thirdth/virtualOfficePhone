bed1f8cfe192335373f4eb2f35377aef641ba8e5
Zack Glaser <zackglaser@gmail.com>
2018-01-13 13:05:23 -0600

    debug

--- messages.php
+++ messages.php
@@ -19,10 +19,8 @@ foreach ($client->calls->read() as $call) {
   /*print_r($call);
   echo "  |  ";
   print_r($call->recordings);
-  echo "  |  ";
-  print_r($call->recordings->read());*/
-  echo " \ ";
-  print_r($call->recordings->properties);
+  echo "  |  ";*/
+  print_r($call->recordings->read());
   echo "<div>
           <p>" . $call->direction . " | " . $call->startTime->date . " | " . $call->from . "</p>
           <a href='recordings.php?callSid=" . $call->sid . "'>Call SID: " . $call->sid . "</a>
