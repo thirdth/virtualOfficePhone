e325c22ddd24358180caf82b0b6eb03bd51f9e8a
Zack Glaser <zackglaser@gmail.com>
2018-01-14 09:10:59 -0600

    digging into call->recordings

--- messages.php
+++ messages.php
@@ -16,8 +16,8 @@ $client = new Client($TWsid, $TWtoken);
 }*/
 
 foreach ($client->calls->read() as $call) {
-  /*print_r($call);
-  echo "  |  ";*/
+  print_r($call->recordings->solution);
+  echo "  |  ";
   print_r($call->recordings);
   echo "  |  ";
   print_r($call->recordings->read());
