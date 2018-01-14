7bc8f9f079479417392415c9e9aa6891643575c3
Zack Glaser <zackglaser@gmail.com>
2018-01-14 09:21:28 -0600

    digging into call->properties

--- messages.php
+++ messages.php
@@ -18,7 +18,7 @@ $client = new Client($TWsid, $TWtoken);
 foreach ($client->calls->read() as $call) {
   var_dump($call);
   echo "  |  \n";
-  print_r($call->recordings);
+  print_r($call->properties);
   echo "  |  \n";
   print_r($call->recordings->read());
   echo "\n<div>
