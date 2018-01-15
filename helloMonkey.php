64a9d99cd7cbe9ccddaf13b1a9df501db55fc1f2
Zack Glaser <zackglaser@gmail.com>
2018-01-15 09:52:50 -0600

    changed alice to woman

--- helloMonkey.php
+++ helloMonkey.php
@@ -3,7 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say voice="alice" language="en-gb">You have reached tech 4 lawyers.</Say>
+    <Say voice="woman" language="en">You have reached tech 4 lawyers.</Say>
     <Gather numDigits="1" action="hello-monkey-transfer.php" method="POST">
       <Say voice="alice" language="en">To speak to a representative, please press 1.</Say>
     </Gather>
