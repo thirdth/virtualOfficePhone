2003f4b26ad9f555eee066f9930596200396e969
Zack Glaser <zackglaser@gmail.com>
2018-01-15 09:49:17 -0600

    changed voice to alice

--- helloMonkey.php
+++ helloMonkey.php
@@ -3,7 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>You have reached tech for lawyers.</Say>
+    <Say voice="alice">You have reached tech for lawyers.</Say>
     <Gather numDigits="1" action="hello-monkey-transfer.php" method="POST">
       <Say>To speak to a representative, please press 1.</Say>
     </Gather>
